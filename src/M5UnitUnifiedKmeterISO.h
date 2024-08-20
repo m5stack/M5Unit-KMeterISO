25cef9a49103d84607c403d554aff93a0494fcef
GOB <goblin52@gmail.com>
2024-08-20 18:59:01 +0900

    Add file

---
+++ src/M5UnitUnifiedKmeterISO.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*!
+  @file M5UnitTVOC.h
+ */
+#ifndef M5_UNIT_UNIFIED_KMETERISO_H
+#define M5_UNIT_UNIFIED_KMETERISO_H
+#ifdef __cplusplus
+#include "M5UnitUnifiedKmeterISO.hpp"
+#else
+#error M5UnitUnifiedKmeterISO requires a C++ compiler, please change file extension to .cc or .cpp
+#endif
+#endif
